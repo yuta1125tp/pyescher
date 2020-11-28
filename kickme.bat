df7201d267758bd1fc90d07457fe4865f260f724
yshira <yuta1125tp@gmail.com>
2020-11-28 21:18:15 +0900

    update readme

--- kickme.bat
+++ kickme.bat
@@ -1,9 +1,7 @@
 python main.py ^
     imgs\source3.jpg ^
-    dst.jpg ^
+    imgs\source3.dst.jpg ^
     config.json ^
     --inverse_mapping ^
     --bg_color 255 255 255 ^
     --pre_resize_scale 0.25
-
-    @REM imgs\EXPO2025.jpg ^
